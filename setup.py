c0f02ec984e8f596225e6f8edda4cd1cecf2613f
Sean Story <sean.j.story@gmail.com>
2024-04-29 09:20:31 -0500

    making it pip intallable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='es_agent_client',
+    version='0.0.1',
+    description='A python implementation of an Elastic Agent Client',
+    url='https://github.com/elastic/python-elastic-agent-client',
+    author='Sean Story',
+    author_email='sean.story@elastic.co',
+    license='Elastic v2',
+    packages=['es_agent_client'],
+    install_requires=[], # TODO
+)
