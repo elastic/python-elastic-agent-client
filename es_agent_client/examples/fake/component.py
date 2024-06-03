37458c73828e09e256e6a7786d9e56f40f9329a7
Sean Story <sean.j.story@gmail.com>
2024-06-03 13:32:50 -0500

    added logging

--- es_agent_client/examples/fake/component.py
+++ es_agent_client/examples/fake/component.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 import sys
+import logging
 
+from es_agent_client.util.logger import logger
 
 def main(args=None):
+    try:
+        run()
+    except Exception as e:
+        logging.exception(e)
+        return 1
+    return 0
+
+
+def run():
     pass
 
 
