18f678d487e1f8a0fb2df7ab52484ea67cbb6a6a
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2024-09-03 16:59:29 +0200

    One more thing

--- elastic_agent_client/examples/fake/component.py
+++ elastic_agent_client/examples/fake/component.py
@@ -3,7 +3,7 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 import asyncio
 import functools
 import signal
