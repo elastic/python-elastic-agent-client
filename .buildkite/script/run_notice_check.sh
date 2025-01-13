158fcf62224e602670db3d297963ac80f0443c16
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-13 13:16:15 +0400

    Update everything

--- .buildkite/script/run_notice_check.sh
+++ .buildkite/script/run_notice_check.sh
@@ -4,23 +4,31 @@
 set -euo pipefail
 
 source .buildkite/script/shared.sh
-source .buildkite/script/git-setup.sh
+
+init_python
 
 make notice
 
 if [ -z "$(git status --porcelain | grep NOTICE.txt)" ]; then
+  echo 'Nothing changed'
   exit 0
 else 
-  if is_pr; then
+  echo 'New changes to NOTICE.txt:'
+  git --no-pager diff
+  if is_pr && ! is_fork; then
+    echo 'Running on a PR that is not a fork, will commit changes'
+    source .buildkite/script/git-setup.sh
     export GH_TOKEN="$VAULT_GITHUB_TOKEN"
 
     git add NOTICE.txt
     git commit -m"Update NOTICE.txt"
     git push
+    sleep 15
+  else
+    echo 'Running against a fork or a non-PR change, skipping pushing changes and just failing instead'
   fi
 
   exit 1
 fi
 
 exit 0
-
