ce13ac3ea3bc55168c9e7e0a63ca4b1475b9ae85
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-13 14:56:46 +0400

    Like this

--- .buildkite/script/run_proto_check.sh
+++ .buildkite/script/run_proto_check.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 source .buildkite/script/shared.sh
 source .buildkite/script/git-setup.sh
 
-make generate SCRIPT_DIR="../scripts"
+SCRIPT_DIR="scripts" make generate 
 
 if [ -z "$(git status --porcelain | grep elastic_agent_client/generated)" ]; then
   exit 0
