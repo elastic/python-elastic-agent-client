465cab5bb37d50ece5b21324b08dc9865237bc72
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-13 13:16:15 +0400

    Update everything

--- .buildkite/script/run_proto_check.sh
+++ .buildkite/script/run_proto_check.sh
@@ -11,12 +11,15 @@ make generate
 if [ -z "$(git status --porcelain | grep elastic_agent_client/generated)" ]; then
   exit 0
 else 
-  if is_pr; then
+  if is_pr && ! is_fork; then
+    echo "Commiting changed protobuf files"
     export GH_TOKEN="$VAULT_GITHUB_TOKEN"
 
     git add elastic_agent_client/generated
     git commit -m"Update protobuf files"
     git push
+  else
+    echo "Running against a fork or non-PR change, skipping committing changes"
   fi
 
   exit 1
