51d7204a3eae71734c00a505a0c5a612550d2b90
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-15 17:45:46 +0400

    Fix notice fix?

--- .buildkite/script/run_notice_check.sh
+++ .buildkite/script/run_notice_check.sh
@@ -5,28 +5,27 @@ set -euo pipefail
 
 source .buildkite/script/shared.sh
 
-make notice
+if is_pr && ! is_fork && ! has_skip_label; then
+  echo 'Skipping autofix'
+  make notice
+else
+  export GH_TOKEN="$VAULT_GITHUB_TOKEN"
+  source .buildkite/script/git-setup.sh
+  make notice
 
-if [ -z "$(git status --porcelain | grep NOTICE.txt)" ]; then
-  echo 'Nothing changed'
-  exit 0
-else 
-  echo 'New changes to NOTICE.txt:'
-  git --no-pager diff
-  if is_pr && ! is_fork && ! has_skip_label; then
-    echo 'Running on a PR that is not a fork, will commit changes'
-    source .buildkite/script/git-setup.sh
-    export GH_TOKEN="$VAULT_GITHUB_TOKEN"
+   if [ -z "$(git status --porcelain | grep NOTICE.txt)" ]; then
+     echo 'Nothing changed'
+     exit 0
+   else
+     echo 'New changes to NOTICE.txt:'
+     git --no-pager diff
 
-    git add NOTICE.txt
-    git commit -m"Update NOTICE.txt"
-    git push
-    sleep 15
-  else
-    echo 'Skipping autofix'
-  fi
+     git add NOTICE.txt
+     git commit -m"Update NOTICE.txt"
+     git push
 
-  exit 1
+     exit 1
+   fi
 fi
 
 exit 0
