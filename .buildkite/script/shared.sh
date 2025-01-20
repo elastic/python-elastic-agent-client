2e728d6e3ab647b783a1448865d1a362355ec7c9
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2025-01-20 16:16:43 +0400

    Delete retry()

--- .buildkite/script/shared.sh
+++ .buildkite/script/shared.sh
@@ -1,24 +1,3 @@
-retry() {
-  local retries=$1; shift
-  local delay=$1; shift
-  local attempts=1
-
-  until "$@"; do
-    retry_exit_status=$?
-    echo "Exited with $retry_exit_status" >&2
-    if (( retries == "0" )); then
-      return $retry_exit_status
-    elif (( attempts == retries )); then
-      echo "Failed $attempts retries" >&2
-      return $retry_exit_status
-    else
-      echo "Retrying $((retries - attempts)) more times..." >&2
-      attempts=$((attempts + 1))
-      sleep "$delay"
-    fi
-  done
-}
-
 is_pr() {
   if [ -z "$BUILDKITE_PULL_REQUEST" ] || [ "$BUILDKITE_PULL_REQUEST" = "false" ]; then
     echo "Running against a non-PR change"
